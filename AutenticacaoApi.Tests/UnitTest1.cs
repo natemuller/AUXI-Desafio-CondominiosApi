7e6ecec34ccf0c92a5e9d9b5a2505d3b073608cc
natan <natan.muller@auxiliadorapredial.com.br>
2026-08-05 12:12:33 -0300

    chore: initial commit

---
+++ AutenticacaoApi.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace AutenticacaoApi.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ CondominiosApi.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CondominiosApi.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
